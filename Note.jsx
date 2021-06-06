5c844c0bfe17272bbeaedc7e267935fce98fe284
tarun9211 <54525241+tarun9211@users.noreply.github.com>
2021-06-06 22:14:58 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Lorem ipsum</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
+        ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis
+        dis parturient.
+      </p>
+    </div>
+  );
+}
+export default Note;
